b364c40f14f059a741955a5f6e53c2d23256cbc8
shuwen <kangshuwen00@gmail.com>
2023-05-22 15:47:49 +0800

    done exercise2

---
+++ exercise2.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <title>exercise2</title>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
+
+
+</head>
+
+<body>
+
+    <?php
+    $number1 = rand(1, 200);
+    $number2 = rand(1, 200);
+
+    if ($number1 > $number2) {
+        echo "<div class = 'col'>";
+        echo "<span class='fs-4 text-primary fw-bold'>$number1</span>";
+
+        echo "<span class= 'text-secondary'></span>" . $number2;
+        echo '</div>';
+    } else if ($number2 > $number1) {
+        echo "<span class= 'text-secondary'></span>" . $number2;
+
+        echo "<span class='fs-4 text-primary fw-bold'>$number1</span>";
+    } else {
+        echo "number same";
+    }
+    ?>
+
+</body>
+
+</html>
