adb73c08928ff364181065de4294db92f8ea91a5
shuwen <kangshuwen00@gmail.com>
2023-05-22 15:47:25 +0800

    done

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+
+    <?php
+    /*
+    echo "My first PHP script!";
+    echo date("l");
+    */
+    ?>
+
+    <?php
+    $color = "red";
+    $COLOR = "black";
+    $coLOR = "blue";
+    echo "My car is " . $color . "<br>";
+    echo "My house is " . $COLOR . "<br>";
+    echo "My boat is " . $coLOR . "<br>";
+    ?>
+
+    <?php
+    $color = "green";
+    $number1 = rand(100, 200);
+    $number2 = rand(100, 200);
+    echo "Random Number" . "<br>";
+    echo "<i>VARIABLES 1:" . $number1 .  $color . "<br>";
+    echo "VARIABLES 2:" . $number2;
+    ?>
+
+
+
+</body>
+
+</html>
