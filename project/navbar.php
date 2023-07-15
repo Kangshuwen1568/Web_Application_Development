914d1129ab91db6fd98caf60b2c77cb1037c9519
shuwen <kangshuwen00@gmail.com>
2023-07-15 19:16:39 +0800

    add dropdown and add all read page in nav menu

--- project/navbar.php
+++ project/navbar.php
@@ -1,6 +1,6 @@
 <nav class="container navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
-        <a class="navbar-brand" href="index.php">E-Commerce</a>
+        <a class="navbar-brand" href="index.php">SW</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -9,12 +9,27 @@
                 <li class="nav-item">
                     <a class="nav-link" href="index.php">Home</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="product_create.php">Product</a>
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Products</a>
+
+                    <ul class="dropdown-menu">
+                        <li> <a class="dropdown-item" href="product_create.php">Create Product</a></li>
+                        <li> <a class="dropdown-item" href="product_read.php">Read Product</a></li>
+                        <li> <a class="dropdown-item" href="product_read_one.php">Read One Product</a></li>
+                    </ul>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="customer_create.php">Customer</a>
+
+                <li class="nav-item  dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Customers</a>
+
+                    <ul class="dropdown-menu">
+                        <li> <a class="dropdown-item" href="customer_create.php">Create Customer</a></li>
+                        <li> <a class="dropdown-item" href="customer_read.php">Read Customer</a></li>
+                        <li> <a class="dropdown-item" href="customer_read_one.php">Read One Customer</a></li>
+                    </ul>
                 </li>
+
                 <li class="nav-item">
                     <a class="nav-link" href="contact_form.php">Contact</a>
                 </li>
