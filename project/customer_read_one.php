f885d26546879aed7800a6a068faf156d66a00c8
shuwen <kangshuwen00@gmail.com>
2023-07-15 19:14:29 +0800

    change title

---
+++ project/customer_read_one.php
@@ -0,0 +1,114 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <title>Read One Record</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+</head>
+
+<body>
+
+    <!-- container -->
+    <div class="container">
+        <?php
+        include 'navbar.php';
+        ?>
+
+        <div class="page-header">
+            <h1>Read Customers</h1>
+        </div>
+
+        <!-- PHP read one record will be here -->
+        <?php
+        // get passed parameter value, in this case, the record ID
+        // isset() is a PHP function used to verify if a value is there or not
+        $id = isset($_GET['id']) ? $_GET['id'] : die('ERROR: Record ID not found.');
+
+        //include database connection
+        include 'config/database.php';
+
+        // read current record's data
+        try {
+            // prepare select query
+            $query = "SELECT id, username, firstname, lastname, gender, date_of_birth, email, registration, account_status FROM customers WHERE id = :id ";
+            $stmt = $con->prepare($query);
+
+            // Bind the parameter
+            $stmt->bindParam(":id", $id);
+
+            // execute our query
+            $stmt->execute();
+
+            // store retrieved row to a variable
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            // values to fill up our form
+            $username = $row['username'];
+            $firstname = $row['firstname'];
+            $lastname = $row['lastname'];
+            $gender = $row['gender'];
+            $date_of_birth = $row['date_of_birth'];
+            $registration_date_time = $row['registration'];
+            $account_status = $row['account_status'];
+            $email = $row['email'];
+            // shorter way to do that is extract($row)
+        }
+
+        // show error
+        catch (PDOException $exception) {
+            die('ERROR: ' . $exception->getMessage());
+        }
+        ?>
+
+        <!-- HTML read one record table will be here -->
+        <!--we have our html table here where the record will be displayed-->
+        <table class='table table-hover table-responsive table-bordered'>
+            <tr>
+                <td>Username</td>
+                <td><?php echo htmlspecialchars($username, ENT_QUOTES);  ?></td>
+            </tr>
+            <tr>
+                <td>First Name</td>
+                <td><?php echo htmlspecialchars($firstname, ENT_QUOTES);  ?></td>
+            </tr>
+            <tr>
+                <td>Last Name</td>
+                <td><?php echo htmlspecialchars($lastname, ENT_QUOTES);  ?></td>
+            </tr>
+            <tr>
+                <td>Gender</td>
+                <td><?php echo htmlspecialchars($gender, ENT_QUOTES);  ?></td>
+            </tr>
+            <tr>
+                <td>Date of Birth</td>
+                <td><?php echo htmlspecialchars($date_of_birth, ENT_QUOTES);  ?></td>
+            </tr>
+            <tr>
+                <td>Registration Time</td>
+                <td><?php echo htmlspecialchars($registration_date_time, ENT_QUOTES);  ?></td>
+            </tr>
+            <tr>
+                <td>Email</td>
+                <td><?php echo htmlspecialchars($email, ENT_QUOTES);  ?></td>
+            </tr>
+            <tr>
+                <td>Account Status</td>
+                <td><?php echo htmlspecialchars($account_status, ENT_QUOTES);  ?></td>
+            </tr>
+
+            <tr>
+                <td></td>
+                <td>
+                    <a href='customer_read.php' class='btn btn-danger'>Back to read products</a>
+                </td>
+            </tr>
+        </table>
+
+
+
+    </div> <!-- end .container -->
+
+</body>
+
+</html>
