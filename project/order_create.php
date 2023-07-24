2792266053a9d816dc023977ec8f20b587790a1c
shuwen <kangshuwen00@gmail.com>
2023-07-24 16:47:15 +0800

    add script

--- project/order_create.php
+++ project/order_create.php
@@ -15,11 +15,6 @@
         <?php
         include 'navbar.php';
         ?>
-        <div class="page-header">
-            <h1>Create New Order</h1>
-        </div>
-
-        <!-- PHP insert code will be here -->
         <?php
         // include database connection
         include 'config/database.php';
@@ -27,20 +22,33 @@
         $stmt = $con->prepare($query);
         $stmt->execute();
         // this is how to get number of rows returned
-        $num = $stmt->rowCount();
+        //$num = $stmt->rowCount();
         $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        ?>
+
+        <div class="page-header">
+            <h1>Create New Order</h1>
+        </div>
+        <!-- PHP insert code will be here -->
+        <?php
 
 
         if ($_POST) {
             // include database connection
-
+            include 'config/database.php';
             $customer_id = $_POST['customer_id'];
             $order_date = $_POST['order_date'];
-            $productid = $_POST['product_id'];
-            $quantities = $_POST['quantity'];
+
+            $product_id = $_POST['product_id'];
+            $quantity = $_POST['quantity'];
 
             // initialize an array to store error messages
             $errors = array();
+
+            // check order date field is empty
+            if (empty($customer_id)) {
+                $errors[] = "Please select your name.";
+            }
 
             // check order date field is empty
             if (empty($order_date)) {
@@ -48,13 +56,13 @@
             }
 
             // Check if at least one product is selected
-            if (empty($productid)) {
-                $errors[] = "Please select at least one product.";
+            if (empty($product_id)) {
+                $errors[] = "Please select product.";
             }
 
             // Check if the quantities are valid (greater than 0)
-            foreach ($quantities as $quantity) {
-                if ($quantity < 1) {
+            foreach ($quantity as $quantitys) {
+                if ($quantitys < 1) {
                     $errors[] = "Quantity must be greater than 0.";
                     break;      //when user input less that 1, error message such display in"errors array", and will stop the foreach(loop)
                 }
@@ -74,39 +82,38 @@
                     // Insert order summary
                     $insert_summary_query = "INSERT INTO order_summary SET customer_id=:customer_id, order_date=:order_date";
                     // prepare query for execution
-                    $stmt = $con->prepare($insert_summary_query);
+                    $stmt_summary = $con->prepare($insert_summary_query);
                     // bind the parameters
-                    $stmt->bindParam(':customer_id', $customer_id);
+                    $stmt_summary->bindParam(':customer_id', $customer_id);
 
                     date_default_timezone_set('Asia/Kuala_Lumpur');
                     $order_date = date('Y-m-d H:i:s');
-                    $stmt->bindParam(':order_date', $order_date);
-
+                    $stmt_summary->bindParam(':order_date', $order_date);
 
                     // Execute the query
-                    if ($stmt->execute()) {
+                    if ($stmt_summary->execute()) {
                         // Get the last inserted order ID
                         $order_id = $con->lastInsertId();
                         // Insert order details
                         $insert_order_details_query = "INSERT INTO order_details SET order_id=:order_id, product_id=:product_id, quantity=:quantity";
                         // prepare query for execution
-                        $stmt = $con->prepare($insert_order_details_query);
+                        $stmt_details = $con->prepare($insert_order_details_query);
 
                         // bind the parameters
-                        for ($i = 0; $i < count($productid); $i++) {
-                            $stmt->bindParam(':order_id', $order_id);
-                            $stmt->bindParam(':product_id', $productid[$i]);
-                            $stmt->bindParam(':quantity', $quantities[$i]);
-                            $stmt->execute();
+                        $sum_product = count($product_id);
+                        for ($i = 0; $i < $sum_product; $i++) {
+                            $stmt_details->bindParam(':order_id', $order_id);
+                            $stmt_details->bindParam(':product_id', $product_id[$i]);
+                            $stmt_details->bindParam(':quantity', $quantity[$i]);
+                            $stmt_details->execute();
                         }
 
                         echo "<div class='alert alert-success'>Order created successfully.</div>";
                     } else {
                         echo "<div class='alert alert-danger'>Unable to create the order.</div>";
                     }
-                }
-                // show error
-                catch (PDOException $exception) {
+                } catch (PDOException $exception) {
+                    echo "<div class='alert alert-danger'>Unable to create the order.</div>";
                     die('ERROR: ' . $exception->getMessage());
                 }
             }
@@ -120,6 +127,7 @@
                 <tr>
                     <td>Customer Name</td>
                     <td><select name='customer_id' class='form-select'>
+                            <option>Please select your username</option>
                             <?php
                             //in "customers" table中得到"customers_name"的data
                             $query = "SELECT id, username FROM customers";
@@ -139,39 +147,20 @@
                         </select>
                     </td>
                 </tr>
-
+            </table>
+            <table class='table table-hover table-responsive table-bordered' id="row_del">
                 <tr>
-                    <td>Product</td>
-                    <td>
-                        Product 1:
-                        <select name="product_id[]" class='form-select'>
-                            <?php
-                            // 下拉菜单的选项
-                            foreach ($products as $product) { // Use $customer['id'] as the value for each option
-                                $productID = $product['id'];
-                                $productName = $product['name'];
-                                echo "<option value='$productID'>$productName</option>";
-                            }
-                            ?>
-                        </select>
-                        Quantity: <input type="number" name="quantity[]" value="0" min="1" class="mb-3">
-                        <br>
-                        Product 2:
-                        <select name="product_id[]" class='form-select '>
-                            <?php
-                            // 下拉菜单的选项
-                            foreach ($products as $product) { // Use $customer['id'] as the value for each option
-                                $productID = $product['id'];
-                                $productName = $product['name'];
-                                echo "<option value='$productID'>$productName</option>";
-                            }
-                            ?>
-                        </select>
-                        Quantity: <input type="number" name="quantity[]" value="0" min="1" class="mb-3">
+                    <td class="text-center text-light">#</td>
+                    <td class="text-center">Product</td>
+                    <td class="text-center">Quantity</td>
+                    <td class="text-center">Action</td>
+                </tr>
+                <tr class="pRow">
+                    <td class="text-center">1</td>
+                    <td class="d-flex">
 
-                        <br>
-                        Product 3:
-                        <select name="product_id[]" class='form-select'>
+                        <select name="product_id[]" class="form-select form-select-lg mb-3 col" aria-label=".form-select-lg example">
+                            <option>Please select a product</option>
                             <?php
                             // 下拉菜单的选项
                             foreach ($products as $product) { // Use $customer['id'] as the value for each option
@@ -181,9 +170,21 @@
                             }
                             ?>
                         </select>
-                        Quantity: <input type="number" name="quantity[]" value="0" min="1" class="mb-3">
+                    </td>
+                    <td><input type="number" class="form-select form-select-lg mb-3" name="quantity[]" aria-label=".form-select-lg example" /></td>
+                    <td><input href='#' onclick='deleteRow(this)' class='btn d-flex justify-content-center btn-danger mt-1' value="Delete" /></td>
+
+                </tr>
+                <tr>
+                    <td>
+
+                    </td>
+                    <td colspan="4">
+                        <input type="button" value="Add More Product" class="btn btn-success add_one" />
                     </td>
                 </tr>
+            </table>
+            <table class='table table-hover table-responsive table-bordered'>
                 <tr>
                     <td>Order Date</td>
                     <td><input type="date" name="order_date" class="form-control" /></td>
@@ -197,8 +198,47 @@
             </table>
         </form>
 
+        <script>
+            document.addEventListener('click', function(event) {
+                if (event.target.matches('.add_one')) {
+                    var rows = document.getElementsByClassName('pRow');
+                    // Get the last row in the table
+                    var lastRow = rows[rows.length - 1];
+                    // Clone the last row
+                    var clone = lastRow.cloneNode(true);
+                    // Insert the clone after the last row
+                    lastRow.insertAdjacentElement('afterend', clone);
+
+                    // Loop through the rows
+                    for (var i = 0; i < rows.length; i++) {
+                        // Set the inner HTML of the first cell to the current loop iteration number
+                        rows[i].cells[0].innerHTML = i + 1;
+                    }
+                }
+            }, false);
+
+            function deleteRow(r) {
+                var total = document.querySelectorAll('.pRow').length;
+                if (total > 1) {
+                    var i = r.parentNode.parentNode.rowIndex;
+                    document.getElementById("row_del").deleteRow(i);
+
+                    var rows = document.getElementsByClassName('pRow');
+                    for (var i = 0; i < rows.length; i++) {
+                        // Set the inner HTML of the first cell to the current loop iteration number
+                        rows[i].cells[0].innerHTML = i + 1;
+                    }
+                } else {
+                    alert("You need order at least one item.");
+                }
+            }
+        </script>
+
 
     </div>
     <!-- end .container -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+</body>
 
 </html>
