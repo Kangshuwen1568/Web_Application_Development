591006cc53bbb120d06345b4a3d991f6fbc2f375
shuwen <kangshuwen00@gmail.com>
2023-08-13 13:07:39 +0800

    modify validate

--- project/menu/validate_login.php
+++ project/menu/validate_login.php
@@ -1,6 +1,7 @@
-<?php 
+<?php
 session_start();
 if (!isset($_SESSION['username'])) {
+    $_SESSION['message'] = 'Please login to access this page!';
     header('location:login.php');
     exit();
 }
