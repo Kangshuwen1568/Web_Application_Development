1c874170df588982b4c71b017ed06597b5efb06e
shuwen <kangshuwen00@gmail.com>
2023-08-19 12:43:39 +0800

    modify database

--- project/index.php
+++ project/index.php
@@ -24,24 +24,147 @@ include 'menu/validate_login.php';
             <div class="page-header">
                 <h1>Welcome to E-commerce System.&#128722;</h1>
             </div>
-            <p>Here is the E-commerce home page.</p>
+            <p></p>
         </div>
-
 
         <!-- PHP insert code will be here -->
         <?php
-        if ($_POST) {
-        }
+        $customer_query = "SELECT * FROM customers";
+        $customer_stmt = $con->prepare($customer_query);
+        $customer_stmt->execute();
+        $customers = $customer_stmt->fetchAll(PDO::FETCH_ASSOC);
 
+        $product_query = "SELECT * FROM products";
+        $product_stmt = $con->prepare($product_query);
+        $product_stmt->execute();
+        $products = $product_stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        $order_summary_query = "SELECT * FROM order_summary";
+        $order_summary_stmt = $con->prepare($order_summary_query);
+        $order_summary_stmt->execute();
+        $order_summary = $order_summary_stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        $order_detail_query = "SELECT * FROM order_details";
+        $order_detail_stmt = $con->prepare($order_detail_query);
+        $order_detail_stmt->execute();
+        $order_details = $order_detail_stmt->fetchAll(PDO::FETCH_ASSOC);
         ?>
+
+        <div class="container row m-auto justify-content-center">
+            <div class="col border border-3 shadow text-center p-3">
+                <h3>Total number of customers</h3>
+                <p class="fs-4"><?php echo count($customers); ?></p>
+            </div>
+            <div class="col border border-3 shadow text-center p-3">
+                <h3>Total number of products</h3>
+                <p class="fs-4"><?php echo count($products); ?></p>
+            </div>
+            <div class="col border border-3 shadow text-center p-3">
+                <h3>Total number of orders</h3>
+                <p class="fs-4"><?php echo count($order_summary); ?></p>
+            </div>
+        </div>
+
+
+        <h2 class="container">Our Order</h2>
+        <div class="container row m-auto gap-3">
+            <div class="col border border-3 shadow p-5 text-center rounded">
+                <h3>Latest Order ID and Summary</h3>
+                <p class="mt-3">Customer Name:
+                    <?php
+                    $latest_order_query = "SELECT order_summary.order_id, order_summary.customer_id, order_summary.order_date, 
+                    SUM(order_details.quantity) AS total_quantity, SUM(CASE WHEN products.promotion_price IS NOT NULL THEN products.promotion_price * order_details.quantity ELSE products.price * order_details.quantity END) AS total_amount 
+                    FROM order_summary JOIN order_details ON order_summary.order_id = order_details.order_id 
+                    JOIN products ON order_details.product_id = products.id
+                        WHERE order_summary.order_id=(SELECT MAX(order_id) FROM order_summary)";
+                    $latest_order_stmt = $con->prepare($latest_order_query);
+                    $latest_order_stmt->execute();
+                    $latest_order = $latest_order_stmt->fetch(PDO::FETCH_ASSOC);
+
+                    $customer_id = $latest_order['customer_id'];
+                    $customer_name_query = "SELECT firstname, lastname FROM customers WHERE id=?";
+                    $customer_name_stmt = $con->prepare($customer_name_query);
+                    $customer_name_stmt->bindParam(1, $customer_id);
+                    $customer_name_stmt->execute();
+                    $customer_name = $customer_name_stmt->fetch(PDO::FETCH_ASSOC);
+                    echo $customer_name['firstname'] . " " . $customer_name['lastname'];
+                    echo "<p><span>Order Date:</span> " . $latest_order['order_date'] . "</p>";
+                    echo "<p><span>Total Quantity:</span> " . $latest_order['total_quantity'] . "</p>";
+                    echo "<p><span>Total Amount:</span> RM " . number_format((float)$latest_order['total_amount'], 2, '.', '') . "</p>";
+                    ?>
+                </p>
+
+            </div>
+            <div class="col border border-3 shadow p-5 text-center rounded">
+                <h3>Highest Purchased Amount Order</h3>
+                <p class="mt-3"><span>Customer Name:</span>
+                    <?php
+                    $highest_order_query = "SELECT order_summary.order_id, order_summary.customer_id, order_summary.order_date, 
+                    SUM(order_details.quantity) AS total_quantity,
+                    SUM(CASE WHEN products.promotion_price IS NOT NULL THEN products.promotion_price * order_details.quantity ELSE products.price * order_details.quantity END) AS total_amount
+                    FROM order_summary JOIN order_details ON order_summary.order_id = order_details.order_id
+                    JOIN products ON order_details.product_id = products.id
+                    GROUP BY order_summary.order_id ORDER BY total_amount DESC LIMIT 1";
+                    $highest_order_stmt = $con->prepare($highest_order_query);
+                    $highest_order_stmt->execute();
+                    $highest_order = $highest_order_stmt->fetch(PDO::FETCH_ASSOC);
+
+                    $customer_id = $highest_order['customer_id'];
+                    $customer_name_query = "SELECT firstname, lastname FROM customers WHERE id=?";
+                    $customer_name_stmt = $con->prepare($customer_name_query);
+                    $customer_name_stmt->bindParam(1, $customer_id);
+                    $customer_name_stmt->execute();
+                    $customer_name = $customer_name_stmt->fetch(PDO::FETCH_ASSOC);
+                    echo $customer_name['firstname'] . " " . $customer_name['lastname'];
+
+                    echo "<p><span>Order Date:</span> " . $highest_order['order_date'] . "</p>";
+                    echo "<p><span>Total Quantity:</span> " . $highest_order['total_quantity'] . "</p>";
+                    echo "<p><span>Total Amount:</span> RM " . number_format((float)$highest_order['total_amount'], 2, '.', '') . "</p>";
+                    ?>
+                </p>
+
+            </div>
+        </div>
+
+        <h2 class="container">Our Product</h2>
+        <div class="container row m-auto gap-3">
+            <div class="col border border-3 shadow p-5 text-center">
+                <h2 class="mb-3">Top 5 Selling Products</h2>
+                <?php
+                $top_product_query = "SELECT order_details.product_id, products.name, SUM(order_details.quantity) AS total_quantity
+                             FROM order_details
+                             JOIN products ON order_details.product_id = products.id
+                             GROUP BY order_details.product_id
+                             ORDER BY total_quantity DESC
+                             LIMIT 5";
+                $top_product_stmt = $con->prepare($top_product_query);
+                $top_product_stmt->execute();
+                $top_products = $top_product_stmt->fetchAll(PDO::FETCH_ASSOC);
+
+                foreach ($top_products as $product) {
+                    echo "<p>{$product['name']} ({$product['total_quantity']} SOLD)</p>";
+                }
+                ?>
+            </div>
+
+            <div class="col border border-3 shadow p-5 text-center">
+                <h2 class="mb-3">3 Products Never Purchased</h2>
+                <?php
+                $no_purchased_product_query = "SELECT id, name FROM products WHERE NOT EXISTS(SELECT product_id FROM order_details WHERE order_details.product_id = products.id)";
+                $no_purchased_product_stmt = $con->prepare($no_purchased_product_query);
+                $no_purchased_product_stmt->execute();
+                $no_purchased_products = $no_purchased_product_stmt->fetchAll(PDO::FETCH_ASSOC);
+
+                foreach ($no_purchased_products as $product) {
+                    echo "<p>{$product['name']}</p>";
+                }
+                ?>
+            </div>
+        </div>
+
+
     </div>
 
 </body>
 
 </html>
-
-<!--referencelink-->
-<!--https://getbootstrap.com/docs/5.3/components/navbar/-->
-<!--https://getbootstrap.com/docs/5.3/content/images/-->
-<!--https://getbootstrap.com/docs/5.3/components/card/-->
-<!--https://getbootstrap.com/docs/5.3/getting-started/introduction/-->
